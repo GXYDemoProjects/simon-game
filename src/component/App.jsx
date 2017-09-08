7ce65b0f73aaf1cabc970edda8c60e72758e86ff
guoxiaoyang <guoxiaoyang0925@qq.com>
2017-09-09 00:33:55 +0800

    add sound play and click effect

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -21,21 +21,58 @@ function SwitchBtn(props) {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      power: false,
+      start: false,
+      strict: false,
+      steps: []
+    };
+  }
+
+  soundPlay(type) {
+    try {
+      let audio = require(`../media/simonSound${type}.mp3`);
+      let sound = new Audio(audio);
+      sound.play().catch(err => console.log('Play Error:', err));
+      setTimeout(() => sound.pause(), 1000);
+    } catch(err) {
+      console.log('Audio Error:', err);
+    }
+  }
+
+  handleMouseDown(type) {
+    return (event) => {
+      this.soundPlay(type);
+      event.preventDefault();
+      event.target.classList.add('lighten');
+    };
+  }
+  handleMouseUp(type) {
+    return (event) => {
+      event.preventDefault();
+      event.target.classList.remove('lighten');
+    };
+  }
+
+  
   render() {
+    
     return (
       <div className="App">
         <h2>Simon Game</h2>
         <div className="play-board">
           <div className="row">
-          <div className="square green"></div>
-          <div className="square red"></div>
+          <div className="square green" onMouseDown={this.handleMouseDown('Green')} onMouseUp={this.handleMouseUp('Green')}></div>
+            <div className="square red" onMouseDown={this.handleMouseDown('Red')} onMouseUp={this.handleMouseUp('Red')}></div>
           </div>
           <div className="row">           
-          <div className="square yellow"></div>
-          <div className="square blue"></div>
+            <div className="square yellow" onMouseDown={this.handleMouseDown('Yellow')} onMouseUp={this.handleMouseUp('Yellow')}></div>
+            <div className="square blue" onMouseDown={this.handleMouseDown('Blue')} onMouseUp={this.handleMouseUp('Blue')}></div>
           </div>
           <div className="circle">
-            <p className="score">11</p>
+            <p className="score">--</p>
           </div>
         </div>
         <div className="control-board">
